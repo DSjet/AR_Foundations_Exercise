f6565b43a618d5fcfe4919aa3f4bc78d8eb6e4a2
DSjet <59322818+DSjet@users.noreply.github.com>
2023-06-06 13:53:41 +0700

    Added new features
    - Added Object Marker Detection
    - Added Object Rotate Animation

---
+++ Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+    [SerializeField] private ARTrackedImageManager _arTrackedImageManager;
+
+    private void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+
+}
